02ed409874e311e0662ba2ed336c984e0824206b
Martinique <me@martidolce.com>
2020-11-19 18:48:32 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,28 @@
+# frozen_string_literal: true
+
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+#
+#
 require 'pry'
 
 class String
+  attr_accessor :sentence, :question
 
   def sentence?
-    
+    end_with?('.')
   end
 
   def question?
-
+    end_with?('?')
   end
 
   def exclamation?
-
+    end_with?('!')
   end
 
   def count_sentences
-
+    split(/[.?!]+/).grep(/\S/).count
   end
 end
